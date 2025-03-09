2c80be2cf881154df10c1009419eea0736aa092b
Amirreza <amirreza.f@780.team>
2025-03-09 11:35:23 +0330

    Feat: add news domain model

---
+++ domain/src/main/java/com/example/domain/model/News.kt
@@ -0,0 +1,23 @@
+package com.example.domain.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Article(
+    val query: String,
+    val author: String,
+    val content: String,
+    val description: String,
+    val publishedAt: String,
+    val source: Source,
+    val title: String,
+    val url: String,
+    val urlToImage: String
+): Parcelable
+
+@Parcelize
+data class Source(
+    val id: String,
+    val name: String
+): Parcelable
