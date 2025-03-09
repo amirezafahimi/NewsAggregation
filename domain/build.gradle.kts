16534873b82e8493612dcabda32e33c96cf0d7a9
Amirreza <amirreza.f@780.team>
2025-03-09 11:33:01 +0330

    Feat: add coroutine and hilt compiler dependencies to domain module

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    id("kotlin-parcelize")
+    id("kotlin-kapt")
 }
 
 android {
@@ -35,10 +37,11 @@ android {
 dependencies {
 
     implementation(projects.core)
+    implementation(projects.data)
 
+    kapt(libs.hilt.compiler)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
