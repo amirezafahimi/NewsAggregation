591e3d6ba3a7a0df3ae2331160b2a5ab82f14b86
Amirreza <amirreza.f@780.team>
2025-03-09 11:35:56 +0330

    Feat: add a mapper to map article dto to article domain model

---
+++ domain/src/main/java/com/example/domain/mapper/Mapper.kt
@@ -0,0 +1,26 @@
+package com.example.domain.mapper
+
+import com.example.data.model.ArticleDto
+import com.example.data.model.SourceDto
+import com.example.domain.model.Article
+import com.example.domain.model.Source
+
+
+fun ArticleDto.toArticle(): Article =
+    Article(
+        author = author ?: "",
+        content = content ?: "",
+        description = description ?: "",
+        publishedAt = publishedAt ?: "",
+        source = source?.toSource() ?: Source("", ""),
+        title = title ?: "",
+        url = url ?: "",
+        urlToImage = urlToImage ?: "",
+        query = query ?: ""
+    )
+
+fun SourceDto.toSource(): Source =
+    Source(
+        id = id ?: "",
+        name = name ?: ""
+    )
