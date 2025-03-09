375446da3f68443357811eda1e90b8b477fa54e6
Amirreza <amirreza.f@780.team>
2025-03-09 11:42:07 +0330

    Feat: complete main activity

--- app/src/main/java/com/example/newsaggregation/MainActivity.kt
+++ app/src/main/java/com/example/newsaggregation/MainActivity.kt
@@ -4,26 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.example.feature.screen.navigation.AppNavGraph
 import com.example.newsaggregation.ui.theme.NewsAggregationTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             NewsAggregationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val navController = rememberNavController()
+                // A surface container using the 'background' color from the theme
+                Box(modifier = Modifier) {
+                    AppNavGraph(navController = navController)
                 }
             }
         }
